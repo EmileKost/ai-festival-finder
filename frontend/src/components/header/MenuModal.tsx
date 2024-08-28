b3832a2719a7ad17cf755732d464a3dd191d342f
Emile Kost <70690100+EmileKost@users.noreply.github.com>
2024-08-28 12:28:40 +0200

    feat: setting up modal structure (wip)

--- frontend/src/components/header/MenuModal.tsx
+++ frontend/src/components/header/MenuModal.tsx
@@ -1,4 +1,12 @@
-export const MenuModal = () => {
+import { motion } from "framer-motion";
+import { Dispatch, SetStateAction } from "react";
+
+type MenuModalProps = {
+	isOpen: boolean;
+	setIsOpen: Dispatch<SetStateAction<boolean>>;
+};
+
+export const MenuModal = ({ isOpen, setIsOpen }: MenuModalProps) => {
 	return (
 		<div>
 			<div>I am the modal</div>
